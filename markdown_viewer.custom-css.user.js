391be8cdd786cf4b3a6a58b8ee16470547e37d33
Roy Ivy III <rivy.dev@gmail.com>
2016-07-28 18:14:36 -0500

    update

--- markdown_viewer.custom-css.user.js
+++ markdown_viewer.custom-css.user.js
@@ -2,7 +2,7 @@
 // @name        markdown_viewer.custom.css.user.js
 // @namespace   com.houseofivy
 //
-// @version     0.1
+// @version     1.01
 // @updateURL   https://raw.githubusercontent.com/rivy/gms-markdown_viewer.custom-css/master/markdown_viewer.custom-css.user.js
 //
 // file extension: .m(arkdown|kdn?|d(o?wn)?)
@@ -31,7 +31,7 @@ link.href = 'data:text/css,'
   + 'body { font-family: Lora, "Noto Serif", "DejaVu Serif", Palatino, serif; font-size: 1em; }'
   + 'body { max-width: 90%; }'
   + 'h1, h2, h3, h4, h5, h6 { font-family: "Fira Sans", "Open Sans", "DejaVu Sans", Calibri, Arial, sans-serif; }'
-  //+ 'h1, h2, h3 { font-weight: 900; }'
+  + 'h1, h2, h3 { font-weight: 900; }'
   + 'code { font-family: "Fira Mono", "Open Sans Mono", "Anonymous Pro", monospace; font-size: 0.8em; }'
   + 'table { width: auto; }'
   ;
